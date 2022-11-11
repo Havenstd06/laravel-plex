9fa8dec20f38ebdb3baf95e1531aa8922ecfd514
Thomas <me@hvs.cx>
2022-11-11 16:38:36 +0100

    Create Sessions Trait

---
+++ src/Traits/PlexAPI/Sessions.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Hav2nstd06\LaravelPlex\Traits\PlexAPI;
+
+use Psr\Http\Message\StreamInterface;
+
+trait Sessions
+{
+    /**
+     * This will retrieve the "Now Playing" Information of the PMS.
+     *
+     * @return array|StreamInterface|string
+     * @throws \Throwable
+     *
+     */
+    public function getNowPlaying(): StreamInterface|array|string
+    {
+        $this->apiEndPoint = "status/sessions";
+
+        $this->verb = 'get';
+
+        return $this->doPlexRequest();
+    }
+
+    /**
+     * Retrieves a listing of all history views.
+     *
+     * @return array|StreamInterface|string
+     * @throws \Throwable
+     *
+     */
+    public function getViewsHistory(): StreamInterface|array|string
+    {
+        $this->apiEndPoint = "status/sessions/history/all";
+
+        $this->verb = 'get';
+
+        return $this->doPlexRequest();
+    }
+}
