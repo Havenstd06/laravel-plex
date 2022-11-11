56fc75f617d5c7aac7a2bd9238257611be954419
Thomas <me@hvs.cx>
2022-11-11 16:39:27 +0100

    Ability to search into database

--- src/Traits/PlexAPI/Databases.php
+++ src/Traits/PlexAPI/Databases.php
@@ -7,15 +7,18 @@ use Psr\Http\Message\StreamInterface;
 trait Databases
 {
     /**
-     * Download databases details.
+     * This will search the in database for the string provided.
      *
+     * @param string $query
      * @return array|StreamInterface|string
-     * @throws \Throwable
      *
+     * @throws \Throwable
      */
-    public function downloadDatabases(): StreamInterface|array|string
+    public function searchDatabase(string $query): StreamInterface|array|string
     {
-        $this->apiEndPoint = "diagnostics/databases";
+        $this->apiEndPoint = "search";
+
+        $this->setRequestQuery('query', $query);
 
         $this->verb = 'get';
 
