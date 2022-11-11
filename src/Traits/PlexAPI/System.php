f3e35352606132083aca4baf6d9975797d5ec3b3
Thomas <me@hvs.cx>
2022-11-11 16:38:46 +0100

    Create System Trait

---
+++ src/Traits/PlexAPI/System.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Hav2nstd06\LaravelPlex\Traits\PlexAPI;
+
+use Psr\Http\Message\StreamInterface;
+
+trait System
+{
+    /**
+     * General plex system information
+     *
+     * @return array|StreamInterface|string
+     * @throws \Throwable
+     *
+     */
+    public function getSystem(): StreamInterface|array|string
+    {
+        $this->apiEndPoint = "system";
+
+        $this->verb = 'get';
+
+        return $this->doPlexRequest();
+    }
+
+    /**
+     * Agents available (and some of their configuration)
+     *
+     * @return array|StreamInterface|string
+     * @throws \Throwable
+     *
+     */
+    public function getSystemAgents(): StreamInterface|array|string
+    {
+        $this->apiEndPoint = "system/agents";
+
+        $this->verb = 'get';
+
+        return $this->doPlexRequest();
+    }
+}
