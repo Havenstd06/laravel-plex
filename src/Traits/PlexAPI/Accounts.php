c4f31da30d78aa90d4143a7ec349559b0cde0b92
Thomas <me@hvs.cx>
2022-11-11 18:59:38 +0100

    Ability to Sign in with Basic Auth or Token

--- src/Traits/PlexAPI/Accounts.php
+++ src/Traits/PlexAPI/Accounts.php
@@ -37,4 +37,36 @@ trait Accounts
 
         return $this->doPlexRequest();
     }
+
+    /**
+     * Use basic auth to Sign in to plex.tv for validating plex username/password and receive an auth token
+     *
+     * @param array $data
+     * @param bool $useHeaderToken
+     *
+     * @return array|StreamInterface|string
+     * @throws \Throwable
+     */
+    public function signIn(array $data, bool $useHeaderToken = false): StreamInterface|array|string
+    {
+        $this->apiBaseUrl = $this->config['plex_tv_api_url'];
+
+        $this->apiEndPoint = "users/sign_in.json";
+
+        if (isset($data['auth']) && (! $useHeaderToken || empty($this->config['token']))) {
+            $this->removeRequestHeader('X-Plex-Token');
+        }
+
+        if (isset($data['auth'])) {
+            $this->options['auth'] = $data['auth'];
+        }
+
+        if (isset($data['headers'])) {
+            $this->setArrayRequestHeader($data['headers']);
+        }
+
+        $this->verb = 'post';
+
+        return $this->doPlexRequest();
+    }
 }
